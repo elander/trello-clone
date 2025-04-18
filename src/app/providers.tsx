b4050765fb8cdc0c85b5a0d28792d182c2e53900
Max Elander <max.elander@mac.com>
2025-04-18 11:00:50 +0200

    Update providers to use CardModalContext

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { CardModal } from "@/components/modals/card-modal";
+import { CardModalProvider } from "@/contexts/CardModalContext";
 
 export function Providers() {
   return (
-    <>
+    <CardModalProvider>
       <CardModal />
-    </>
+    </CardModalProvider>
   );
 }
