ac4ec592408ab5512909067c5b7b89dbdf44f3e9
Max Elander <max.elander@mac.com>
2025-05-03 11:53:27 +0200

    Add drizzle config file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
-  driver: 'better-sqlite3',
+  driver: 'better-sqlite',
   dbCredentials: {
     url: join(process.cwd(), 'sqlite.db'),
   },
