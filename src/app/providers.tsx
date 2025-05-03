1e3e9f0f593193e25fd483dd490326577fd94448
Max Elander <max.elander@mac.com>
2025-05-03 12:02:29 +0200

    Update Providers to pass down children

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,9 +3,14 @@
 import { CardModal } from "@/components/modals/card-modal";
 import { CardModalProvider } from "@/contexts/CardModalContext";
 
-export function Providers() {
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
   return (
     <CardModalProvider>
+      {children}
       <CardModal />
     </CardModalProvider>
   );
